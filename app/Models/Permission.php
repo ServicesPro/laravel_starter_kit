8419ebd682ed6c1ca8db1f98c0d5ba0ad04f2a4a
Stéphane KUMA <kumastephane@gmail.com>
2021-06-11 03:54:21 +0000

    Module, Permission & Role

---
+++ app/Models/Permission.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Role;
+use App\Models\Module;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    public function module()
+    {
+        return $this->belongsTo(Module::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+}
